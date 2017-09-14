f153c7bef22b57531f664d73443a08ce0e2a284b
Mike Martinsky <michaelmartinsky@yahoo.com>
2017-09-13 22:40:30 -0400

    added test script to package.json

--- lambda.js
+++ lambda.js
@@ -1,3 +1,9 @@
+var Twitter = require('twitter');
+
 exports.handler = function (event, context) {
     context.succeed('hello world');
+    // get random player from google sheets
+    // create text for tweet
+    // tweet it 
+    // set player to used
 };
