96a92b04da7d3dad6d17bc0ae71fcc62af058978
Mike Martinsky <michaelmartinsky@yahoo.com>
2017-09-17 20:49:12 -0400

    getPlayer() method

--- lambda.js
+++ lambda.js
@@ -2,9 +2,31 @@ var Twitter = require('twitter');
 var Xlsx = require('xlsx');
 
 exports.handler = function (event, context) {
-    context.succeed('hello world');
-    // get random player from google sheets
+    var player = getPlayer();
+    console.log(player);
+    // get random player from spreadsheet
     // create text for tweet
     // tweet it 
     // set player to used
 };
+
+function getPlayer() {
+    var wb = Xlsx.readFile('RememberSomeGuys.csv');
+    var jsObj = Xlsx.utils.sheet_to_json(wb.Sheets['Sheet1']);
+    var index = -1;
+    var ctr = 0;
+    while (index === -1 && ctr < jsObj.length) {
+        index = parseInt(Math.random() * jsObj.length);
+        if (jsObj[index].Used !== 'N') {
+            index = -1;
+        }
+        ctr++;
+    }
+    if (index === -1) {
+        throw new Error("No unused players remaining. Please add more guys to remember and congratulations on a successful bot");
+    } else {
+        return jsObj[index];
+    }
+}
+
+exports.handler(null, null);
