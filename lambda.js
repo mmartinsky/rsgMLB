64c686d98e903648a360a310842088e05d0007bc
Mike Martinsky <michaelmartinsky@yahoo.com>
2017-09-17 21:32:20 -0400

    createTextForTweet

--- lambda.js
+++ lambda.js
@@ -2,13 +2,25 @@ var Twitter = require('twitter');
 var Xlsx = require('xlsx');
 
 exports.handler = function (event, context) {
-    var player = getPlayer();
-    console.log(player);
     // get random player from spreadsheet
+    var player = getPlayer();
     // create text for tweet
+    var textForTweet = createTextForTweet(player);
+    console.log(textForTweet);
     // tweet it 
+
     // set player to used
 };
+
+function createTextForTweet(player) {
+    var result = "Remember " + player.Name + "?\n";
+    result += player.Start + " - " + player.End + "\n";
+    result += "AVG: " + player.AVG.substring(1) + ", HR: " + player.HR + ", RBI: " + player.RBI + ", WAR: " + player.WAR + "\n";
+    result += 'http://www.fangraphs.com/statss.aspx?playerid=' + player.playerid;
+    result += "\n#rsgMLB";
+    console.log(result.length);
+    return result;
+}
 
 function getPlayer() {
     var wb = Xlsx.readFile('RememberSomeGuys.csv');
@@ -17,7 +29,7 @@ function getPlayer() {
     var ctr = 0;
     while (index === -1 && ctr < jsObj.length) {
         index = parseInt(Math.random() * jsObj.length);
-        if (jsObj[index].Used !== 'N') {
+        if (jsObj[index].Used !== 'N' || jsObj[index].Start === undefined) {
             index = -1;
         }
         ctr++;
