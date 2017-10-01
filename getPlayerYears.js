0e895dc40a0a2854f4f2317ef11ea87b9fb562b8
Mike Martinsky <michaelmartinsky@yahoo.com>
2017-10-01 19:42:54 -0400

    update player Used post player picture
    removed xlsx
    added aws sdk using s3

--- getPlayerYears.js
+++ getPlayerYears.js
@@ -34,10 +34,10 @@ function populatePlayerYears() {
                     console.log("Success: " + JSON.parse(resp.config.data).playerName);
                     Xlsx.writeFile(this.wb, 'RememberSomeGuys.csv');
                 }).catch((err) => {
-                    //console.log("Failed: " + JSON.parse(err.config.data).playerName);
+                    console.log("Failed: " + JSON.parse(err.config.data).playerName + " - " + err.message);
                 });
             }
-        }, 10000, i);
+        }, 1000, i);
     }
 }
 
--- lambda.js
+++ lambda.js
@@ -1,14 +1,30 @@
 var Twitter = require('twit');
-var Xlsx = require('xlsx');
 var i2b = require("imageurl-base64");
+var aws = require('aws-sdk');
+var csv = require('csvtojson')
+
+
+var jsObj;
+var index;
 
 exports.handler = function () {
-    // get random player from spreadsheet
-    var player = getPlayer();
-    // create text for tweet
-    var textForTweet = createTextForTweet(player);
-    console.log(textForTweet);
-    // tweet it 
+    var fileProm = downloadFile();
+    fileProm.then(function (data) {
+        var playerPromise = getPlayer(data.Body.toString('utf-8'));
+        playerPromise.then(function (data) {
+            console.log(data);
+            var player = data;
+            var textForTweet = createTextForTweet(player);
+            console.log(textForTweet);
+            getPictureAndPostTweet(player, textForTweet);
+        });
+    }).catch(function (err) {
+        console.log("error");
+        console.log(err);
+    });
+};
+
+function getPictureAndPostTweet(player, textForTweet) {
     var client = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -16,7 +32,7 @@ exports.handler = function () {
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 
     });
-    i2b("http://mlb.mlb.com/assets/images/1/0/6/102773106/Angelsjerz_iksbc8u6.jpg", function (err, data) {
+    i2b(player.Picture, function (err, data) {
         if (err) {
             console.error("error converting image to base64");
             console.error(err);
@@ -48,43 +64,86 @@ exports.handler = function () {
                                 media_ids: [mediaIdStr]
                             }
 
-                            client.post('statuses/update', params, function () {
-                                console.error("Success");
-                            })
+                            client.post('statuses/update', params, function (err) {
+                                if (err) {
+                                    console.log("Error posting Tweet");
+                                    console.log(err);
+                                } else {
+                                    console.log("Tweet Success");
+                                    jsObj[index].Used = 'Y';
+                                    var jsonStringCsv = JSON.stringify(jsObj);
+                                    var params = {
+                                        Body: jsonStringCsv,
+                                        Bucket: "rsg-mlb",
+                                        Key: "RememberSomeGuys.csv",
+                                        ContentType: "text/csv"
+                                    };
+                                    var client = new aws.S3({
+                                        accessKeyId: process.env.AWS_KEY,
+                                        secretAccessKey: process.env.AWS_SECRET
+                                    });
+                                    client.putObject(params, function (err, data) {
+                                        if (err) {
+                                            console.log(err, err.stack);
+                                        } else {
+                                            console.log(data);
+                                        }
+                                    });
+                                }
+                            });
                         }
                     });
                 }
             });
         }
     });
-};
+}
 
 function createTextForTweet(player) {
     var result = "Remember " + player.Name + "?\n";
     result += player.Start + " - " + player.End + ". " + "AVG: " + player.AVG.substring(1) + ", HR: " + player.HR + ", RBI: " + player.RBI + ", WAR: " + player.WAR + "\n";
     result += 'http://www.fangraphs.com/statss.aspx?playerid=' + player.playerid;
-    result += "\n#rsgMLB";
+    result += "\n#rsgMLB #MLB #baseball";
     console.log(result.length);
     return result;
 }
 
-function getPlayer() {
-    var wb = Xlsx.readFile('RememberSomeGuys.csv');
-    var jsObj = Xlsx.utils.sheet_to_json(wb.Sheets['Sheet1']);
-    var index = -1;
-    var ctr = 0;
-    while (index === -1 && ctr < jsObj.length) {
-        index = parseInt(Math.random() * jsObj.length);
-        if (jsObj[index].Used !== 'N' || jsObj[index].Start === undefined) {
-            index = -1;
+function getPlayer(body) {
+    var p = new Promise(function (resolve, reject) {
+        csv()
+            .fromString(body)
+            .on('end_parsed', function (jsonArrObj) {
+                jsObj = jsonArrObj;
+                resolve("Success");
+            });
+    });
+    return p.then(function (data) {
+        index = -1;
+        var ctr = 0;
+        while (index === -1 && ctr < jsObj.length * 2) {
+            index = parseInt(Math.random() * jsObj.length);
+            if (jsObj[index].Used !== 'N' || jsObj[index].Start === "" || jsObj[index].Picture === "") {
+                index = -1;
+            }
+            ctr++;
         }
-        ctr++;
-    }
-    if (index === -1) {
-        throw new Error("No unused players remaining. Please add more guys to remember and congratulations on a successful bot");
-    } else {
-        return jsObj[index];
-    }
+        if (index === -1) {
+            throw new Error("No unused players remaining. Please add more guys to remember and congratulations on a successful bot");
+        } else {
+            return jsObj[index];
+        }
+    });
+}
+
+function downloadFile() {
+    var client = new aws.S3({
+        accessKeyId: process.env.AWS_KEY,
+        secretAccessKey: process.env.AWS_SECRET
+    });
+    return client.getObject({
+        Bucket: 'rsg-mlb',
+        Key: 'RememberSomeGuys.csv'
+    }).promise();
 }
 
 exports.handler(null, null);
